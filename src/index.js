4b92038d2f9f915bd3ffadd97d7b072c7d34d9d7
John Carney <jcarney000@gmail.com>
2017-04-11 19:17:18 -0700

    add simple launch intent; add more info intent to schema

--- src/index.js
+++ src/index.js
@@ -9,9 +9,26 @@ exports.handler = function (event, context, callback) {
   if (typeof process.env.DEBUG === 'undefined') {
     alexa.appId = appId
   }
-
-  alexa.registerHandlers(handlers)
+  alexa.dynamoDBTableName = 'tableName'
+  alexa.registerHandlers(launchHandlers)
   alexa.execute()
 }
 
-const handlers = {}
+const states = {
+  RESULTS: '_RESULTS',
+  SEARCHING: '_SEARCHING'
+}
+
+const launchHandlers = {
+  'LaunchRequest': function () {
+    if (!this.attributes['address']) {
+      this.attributes['speechOutput'] = 'Welcome to MyRep. If you\'d like to set a default address, you can say, "set my address to," followed by your address. Or you can ask for the representatives for a particular address.'
+      this.attributes['repromptOutput'] = 'Sorry, I didn\'t catch that. You can ask for the representatives for a particular address, or you can set a new default search address.'
+    } else {
+      this.attributes['speechOutput'] = 'Welcome to MyRep. You can ask for the representatives for your address, or you can ask for the representatives for a particular address.'
+      this.attributes['repromptOutput'] = 'Sorry, I didn\'t catch that. You can ask for the representatives for your saved address, a particular address, or you can set a new default search address.'
+    }
+
+    this.emit(':ask', this.attributes['speechOutput'], this.attributes['repromptOutput'])
+  }
+}
