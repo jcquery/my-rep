1d3a1a2763c13a88d86fea5d9155ab2b7aa0506b
John Carney <jcarney000@gmail.com>
2017-04-21 15:29:29 -0700

    create basic rep search

--- src/handlers/helpers.js
+++ src/handlers/helpers.js
@@ -1,3 +1,36 @@
 'use strict'
 
-exports.helpers = {}
+const axios = require('axios')
+const dotenv = require('dotenv').config()
+
+const helpers = {
+  addressSearch: function (address, office) {
+    let roles
+    const key = process.env.CIVICS_KEY
+
+    if (office === 'house') {
+      roles = 'legislatorLowerBody'
+    } else if (office === 'senate') {
+      roles = 'legislatorUpperBody'
+    } else {
+      roles = 'legislatorLowerBody, legislatorUpperBody'
+    }
+
+    axios.get('https://www.googleapis.com/civicinfo/v2/representatives', {
+      params: {
+        levels: 'country',
+        roles,
+        key,
+        address
+      }
+    })
+    .then((res) => {
+      console.log(res.data.officials)
+    })
+    .catch((err) => {
+      console.log(err.response.data)
+    })
+  }
+}
+
+exports.helpers = helpers
