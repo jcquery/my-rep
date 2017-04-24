47b727df9ae3ae9cfa97a3fc0aa5b0ac95a02b9b
John Carney <jcarney000@gmail.com>
2017-04-23 20:04:06 -0700

    add a script to generate a map of congressional bioguide ids and a list of names

---
+++ src/generate.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const fsp = require('fs-promise')
+const axios = require('axios')
+const dotenv = require('dotenv').config()
+const path = require('path')
+
+function getSenators(congress) {
+  return axios.get(`https://api.propublica.org/congress/v1/${congress}/senate/members.json`, {
+    headers: {
+      'X-API-Key': process.env.SUNLIGHT_KEY
+    }
+  })
+}
+function getReps(congress) {
+  return axios.get(`https://api.propublica.org/congress/v1/${congress}/house/members.json`, {
+    headers: {
+      'X-API-Key': process.env.SUNLIGHT_KEY
+    }
+  })
+}
+function buildFiles (conArray) {
+  return conArray.reduce((obj, mem) => {
+    const name = `${mem.first_name} ${mem.last_name}`
+    if (obj.idMap[name]) {
+      obj.idMap[name].push(mem.id)
+    } else {
+      obj.nameList.push(name)
+      obj.idMap[name] = [mem.id]
+    }
+
+    return obj
+  }, {nameList: [], idMap: {}})
+}
+function outputFiles(conNum) {
+  let obj
+
+  axios.all([getSenators(conNum), getReps(conNum)])
+    .then(axios.spread((senators, reps) => {
+      return buildFiles(senators.data.results[0].members.concat(reps.data.results[0].members))
+    }))
+    .then((built) => {
+      obj = built
+      return fsp.writeFile(path.join(__dirname, '../speechAssets/customSlotTypes/REP_NAME.txt'), obj.nameList.join('\n'))
+    })
+    .then(() => {
+      return fsp.writeFile(path.join(__dirname, '/handlers/congressMap.json'), JSON.stringify(obj.idMap))
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+outputFiles(115)
+
+// axios.get('https://api.propublica.org/congress/v1/115/senate/members.json', {
+//   headers: {
+//     'X-API-Key': process.env.SUNLIGHT_KEY
+//   }
+// })
+//   .then((res) => {
+//     return res.data.results[0].members.map((member) => {
+//       return `${member.first_name} ${member.last_name}`
+//     }).join('\n')
+//   })
+//   .then((list) => {
+//     fsp.writeFile(path.join(__dirname, '../speechAssets/customSlotTypes/REP_NAME.txt'), list)
+//   })
+//   .catch((err) => {
+//     console.log(err.response || err)
+//   })
+
