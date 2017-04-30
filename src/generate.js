fd53784595c719d05e2d5bb6bf647835b51304ef
John Carney <jcarney000@gmail.com>
2017-04-30 16:36:57 -0700

    linted

--- src/generate.js
+++ src/generate.js
@@ -5,14 +5,14 @@ const axios = require('axios')
 const dotenv = require('dotenv').config()
 const path = require('path')
 
-function getSenators(congress) {
+function getSenators (congress) {
   return axios.get(`https://api.propublica.org/congress/v1/${congress}/senate/members.json`, {
     headers: {
       'X-API-Key': process.env.SUNLIGHT_KEY
     }
   })
 }
-function getReps(congress) {
+function getReps (congress) {
   return axios.get(`https://api.propublica.org/congress/v1/${congress}/house/members.json`, {
     headers: {
       'X-API-Key': process.env.SUNLIGHT_KEY
@@ -32,7 +32,7 @@ function buildFiles (conArray) {
     return obj
   }, {nameList: [], idMap: {}})
 }
-function outputFiles(conNum) {
+function outputFiles (conNum) {
   let obj
 
   axios.all([getSenators(conNum), getReps(conNum)])
@@ -52,21 +52,3 @@ function outputFiles(conNum) {
 }
 
 outputFiles(115)
-
-// axios.get('https://api.propublica.org/congress/v1/115/senate/members.json', {
-//   headers: {
-//     'X-API-Key': process.env.SUNLIGHT_KEY
-//   }
-// })
-//   .then((res) => {
-//     return res.data.results[0].members.map((member) => {
-//       return `${member.first_name} ${member.last_name}`
-//     }).join('\n')
-//   })
-//   .then((list) => {
-//     fsp.writeFile(path.join(__dirname, '../speechAssets/customSlotTypes/REP_NAME.txt'), list)
-//   })
-//   .catch((err) => {
-//     console.log(err.response || err)
-//   })
-
